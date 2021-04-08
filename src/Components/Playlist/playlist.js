bfa20cb78e822ee056e9611d669f6f17d267a694
supminn <30731236+supminn@users.noreply.github.com>
2021-04-08 18:13:48 +0530

    fix: watch later non-editable

--- src/Components/Playlist/playlist.js
+++ src/Components/Playlist/playlist.js
@@ -22,7 +22,7 @@ export const Playlist = () => {
             <>
               <b className="txt-header-3">Empty Playlist! 🤨</b>
               <Link to="/" className="no-line">
-                <button className="btn btn-primary">Add Videos</button> 
+                <button className="btn btn-primary">Add Videos</button>
               </Link>
             </>
           )}
@@ -36,45 +36,52 @@ const PlaylistHeader = ({ name, listId }) => {
   const [listName, setListName] = useState(name);
   const [editable, setEditable] = useState(false);
 
-  const {dispatch} = useDataContext();
+  const { dispatch } = useDataContext();
 
   const updateName = () => {
-    dispatch({type:"RENAME_PLAYLIST",payload:{listId, listName}})
+    dispatch({ type: "RENAME_PLAYLIST", payload: { listId, listName } });
     setEditable(false);
-  }
-  
+  };
+
   const deleteList = () => {
-    dispatch({type:"DELETE_PLAYLIST",payload:listId});
-  }
+    dispatch({ type: "DELETE_PLAYLIST", payload: listId });
+  };
 
   const discardChanges = () => {
     setListName(name);
     setEditable(false);
-  }
+  };
 
   return (
     <div className="playlist-icon-container">
-      <input
+      {listId !== 1?<input
         type="text"
         value={listName}
         onChange={(e) => setListName(e.target.value)}
         onFocus={() => setEditable(true)}
         className={editable ? "playlist-edited" : "playlist-name"}
-      />
-      <i onClick={() => editable?updateName():setEditable(true)}
-        className={`primaryBg-txt fas fa-lg ${
-          editable ? "fa-check-circle" : "fa-edit"
-        }`}
-      >
-        <span className="badge-icon hidden-vis"></span>
-      </i>
-      <i onClick={()=>editable?discardChanges():deleteList()}
-        className={`secondary-txt fas fa-lg ${
-          editable ? "fa-times-circle" : "fa-trash"
-        }`}
-      >
-        <span className="badge-icon hidden-vis"></span>
-      </i>
+      />:
+      <h3 className="playlist-name">{name}</h3>}
+      {listId !== 1 && (
+        <>
+          <i
+            onClick={() => (editable ? updateName() : setEditable(true))}
+            className={`primaryBg-txt fas fa-lg ${
+              editable ? "fa-check-circle" : "fa-edit"
+            }`}
+          >
+            <span className="badge-icon hidden-vis"></span>
+          </i>
+          <i
+            onClick={() => (editable ? discardChanges() : deleteList())}
+            className={`secondary-txt fas fa-lg ${
+              editable ? "fa-times-circle" : "fa-trash"
+            }`}
+          >
+            <span className="badge-icon hidden-vis"></span>
+          </i>
+        </>
+      )}
     </div>
   );
 };
